531c8b6d803570c4b0846edf3de2cbcccab1178f
Denis Kontek <deniskontek@securesouls.com>
2026-09-23 19:26:34 +0200

    feat!: drop C++ and JavaScript/TypeScript support
    BREAKING CHANGE: The API is now Rust-only. C++ and JavaScript/TypeScript bindings have been removed.

--- C++/binding.gyp
+++
@@ -1,8 +0,0 @@
-{
-  "targets": [
-    {
-      "target_name": "SoulAPI",
-      "sources": [ "SoulAPI.c" ]
-    }
-  ]
-}
--- TypeScript/Data/functions/ipFromSocket.ts
+++
@@ -1,5 +0,0 @@
-import { Socket } from 'node:net';
-
-const ipFromSocket = (s: Socket): string => (s.remoteAddress ?? '').replace(/^::ffff:/, '');
-
-export default ipFromSocket;
