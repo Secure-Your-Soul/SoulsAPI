02f997a8b3112126debcf48cb5134d09ec68ec5a
Denis Kontek <deniskontek@securesouls.com>
2026-09-25 13:45:56 +0200

    feat: initial Rust workspace files
    - add api crate (facade)
    - add http crate
    - add crypto crate
    - add `hi()` function in http, crypto crates
    - configure Cargo.toml in root and crates

---
+++ Rust/api/src/lib.rs
@@ -0,0 +1,4 @@
+#[cfg(feature = "http")]
+pub use api_http as http;
+#[cfg(feature = "crypto")]
+pub use api_crypto as crypto;
---
+++ Rust/crypto/src/lib.rs
@@ -0,0 +1,4 @@
+#[warn(unused)]
+pub fn hi() {
+    println!("Hello Crypto");
+}
